ba0149548dc23799ef8fc8941878f135ae646faf
Obtains <97128583+Obtains@users.noreply.github.com>
2022-06-25 11:52:47 -0400

    changed ports

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-    console.log("App is running on port " + PORT);
-});
+// server.listen(PORT, () => {
+//     console.log("App is running on port " + PORT);
+// });
 
 
 const hbs = exphbs.create({ helpers });
